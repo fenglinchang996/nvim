ab8fc18751cb8698c080fc51dfae9d303091be71
FLC <paulespling@gmail.com>
2026-05-31 15:46:03 +0800

    feat: remove duplicated config

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,8 +1,5 @@
 return {
   "akinsho/bufferline.nvim",
-  keys = {
-    { "<leader>bj", "<Cmd>BufferLinePick<CR>", desc = "Jump to Buffer" },
-  },
   opts = {
     options = {
       always_show_bufferline = true,
